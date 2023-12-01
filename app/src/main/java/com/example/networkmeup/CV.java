a2b1a3221c92ebbcffc5b3d25bcab0a432bf2b45
Gaspar <rigos.gasparis@gmail.com>
2023-12-01 19:32:18 +0200

    created CV class with CV junit tests

---
+++ app/src/main/java/com/example/networkmeup/CV.java
@@ -0,0 +1,60 @@
+package com.example.networkmeup;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class CV {
+    private String additionalSkillset;
+    private ArrayList<Education> education;
+    private ArrayList<WorkExperience> workExperiences;
+    private ArrayList<LanguageKnowledge> languageKnowledge;
+
+
+    public CV(){
+        education = new ArrayList<>();
+        workExperiences = new ArrayList<>();
+        languageKnowledge = new ArrayList<>();
+    }
+
+    private void validateInput(Object obj){
+        if (obj == null){
+            throw new NullPointerException("Field cannot be null.");
+        }
+    }
+
+    public String getAdditionalSkillset() {
+        return additionalSkillset;
+    }
+
+    public void setAdditionalSkillset(String additionalSkillset) {
+        validateInput(additionalSkillset);
+        this.additionalSkillset = additionalSkillset;
+    }
+
+    public ArrayList<Education> getEducation() {
+        return education;
+    }
+
+    public void setEducation(Education education){
+        validateInput(education);
+        this.education.add(education);
+    }
+
+    public ArrayList<WorkExperience> getWorkExperiences() {
+        return workExperiences;
+    }
+    public void setWorkExperience(WorkExperience workExperience){
+        validateInput(workExperience);
+        this.workExperiences.add(workExperience);
+    }
+
+    public ArrayList<LanguageKnowledge> getLanguageKnowledge() {
+        return languageKnowledge;
+    }
+
+    public void setLanguageKnowledge(LanguageKnowledge languageKnowledge){
+        validateInput(languageKnowledge);
+        this.languageKnowledge.add(languageKnowledge);
+    }
+
+}
---
+++ app/src/test/java/com/example/networkmeup/CVTest.java
@@ -0,0 +1,64 @@
+package com.example.networkmeup;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CVTest {
+    private CV cv;
+
+    @Before
+    public void setup(){
+        cv = new CV();
+    }
+
+    @Test
+    public void nullAdditionalSkillsetCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            cv.setAdditionalSkillset(null);
+        });
+    }
+    @Test
+    public void nullEducationCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            cv.setEducation(null);
+        });
+    }
+    @Test
+    public void nullWorkExperienceCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            cv.setWorkExperience(null);
+        });
+    }
+
+    @Test
+    public void nullLanguageKnowledgeCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            cv.setLanguageKnowledge(null);
+        });
+    }
+
+    @Test
+    public void validAdditionalSkillsetCheck(){
+        cv.setAdditionalSkillset("Very cooperative");
+        Assert.assertEquals("Very cooperative", cv.getAdditionalSkillset());
+    }
+    @Test
+    public void validEducationCheck(){
+        Education edu = new Education("High School diploma", new ExpertiseArea("Economics"), LevelOfStudies.High_School);
+        cv.setEducation(edu);
+        Assert.assertEquals("High School diploma", cv.getEducation().get(0).getDescription());
+    }
+    @Test
+    public void validWorkExperienceCheck(){
+        WorkExperience workExp = new WorkExperience(2, "Summer part time job", new ExpertiseArea("Serving"));
+        cv.setWorkExperience(workExp);
+        Assert.assertEquals("Summer part time job", cv.getWorkExperiences().get(0).getDescription());
+    }
+    @Test
+    public void validLanguageKnowledgeCheck(){
+        LanguageKnowledge langKnow = new LanguageKnowledge("6 years of studies", new Language("French"), LevelOfKnowledge.Proficiency);
+        cv.setLanguageKnowledge(langKnow);
+        Assert.assertEquals("6 years of studies", cv.getLanguageKnowledge().get(0).getDescription());
+    }
+}
