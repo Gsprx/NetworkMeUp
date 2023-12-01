61955c2c5af0174dcb7e743b6e99e27d19477984
Gaspar <rigos.gasparis@gmail.com>
2023-12-01 21:24:15 +0200

    changes to class diagram and image

---
+++ app/src/main/java/com/example/networkmeup/Application.java
@@ -0,0 +1,12 @@
+package com.example.networkmeup;
+
+public class Application {
+    private Employee applicant;
+    private static Integer classCount;
+    private Integer ID;
+    private boolean status;
+    private String coverLetter;
+    public Application(Employee applicant, String coverLetter,){
+
+    }
+}
