58aa10f68d35f6693dd9bf1a42a32e6e0db24975
Gaspar <rigos.gasparis@gmail.com>
2023-12-01 21:47:03 +0200

    created Application class with junit tests, updated class diagram to match class implementation

--- app/src/main/java/com/example/networkmeup/Application.java
+++ app/src/main/java/com/example/networkmeup/Application.java
@@ -2,11 +2,53 @@ package com.example.networkmeup;
 
 public class Application {
     private Employee applicant;
-    private static Integer classCount;
+    private static Integer classCount = 0;
     private Integer ID;
     private boolean status;
     private String coverLetter;
-    public Application(Employee applicant, String coverLetter,){
 
+
+    public Application(Employee applicant, String coverLetter){
+        validateData(applicant, coverLetter);
+        this.applicant = applicant;
+        this.coverLetter = coverLetter;
+        classCount++;
+        this.ID = classCount;
+    }
+
+    private void validateData(Employee employee, String coverLetter){
+        if(employee == null){
+            throw new NullPointerException("Employee field cannot be null.");
+        }
+        if(coverLetter == null){
+            throw new NullPointerException("Cover letter field cannot be null.");
+        }
+        return;
+    }
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Employee getEmployee() {
+        return applicant;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+
+    //ONLY FOR TESTING PURPOSES!!!
+    public void setID(Integer id){
+        this.ID = id;
+    }
+    //ONLY FOR TESTING PURPOSES!!!
+
+    public String getCoverLetter() {
+        return coverLetter;
     }
 }
---
+++ app/src/test/java/com/example/networkmeup/ApplicationTest.java
@@ -0,0 +1,52 @@
+package com.example.networkmeup;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApplicationTest {
+    private Application app;
+
+    @Before
+    public void setup(){
+        app = new Application(new Employee(new Email("employ@example.com"),
+                new Phone("5658274859"), new Password("Test1234!")), "cover letter test");
+    }
+
+    @Test
+    public void nullEmployeeCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            app = new Application(null,"cover letter");
+        });
+    }
+
+    @Test
+    public void nullCoverLetterCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            app = new Application(new Employee(new Email("employ@example.com"),
+                    new Phone("5658274859"), new Password("Test1234!")),null);
+        });
+    }
+
+    @Test
+    public void validStatusCheck(){
+        app.setStatus(true);
+        Assert.assertEquals(true, app.getStatus());
+    }
+    @Test
+    public void validIDCheck(){
+        app.setID(10);
+        Assert.assertEquals((int)10, (int)app.getID());
+    }
+
+    @Test
+    public void validEmployeeCheck(){
+        Assert.assertEquals("employ@example.com", app.getEmployee().getEmail().getAddress());
+    }
+
+    @Test
+    public void validCoverLetterCheck(){
+        Assert.assertEquals("cover letter test", app.getCoverLetter());
+    }
+
+}
