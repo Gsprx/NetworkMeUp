ff67b9e9d6fe6f8785936a3b2db03a16caf3b0c8
Gaspar <rigos.gasparis@gmail.com>
2023-12-02 15:59:40 +0200

    Finished Android Studio part of R3, all classes 100% coverage. Created Employer and junit tests

--- app/src/main/java/com/example/networkmeup/Employer.java
+++ app/src/main/java/com/example/networkmeup/Employer.java
@@ -1,2 +1,100 @@
-package com.example.networkmeup;public class Employer {
+package com.example.networkmeup;
+
+import java.util.ArrayList;
+
+public class Employer {
+    private String companyName;
+    private String sector;
+    private Email email;
+    private Phone phone;
+    private Password password;
+    private TIN tin;
+    private ArrayList<Job> jobs;
+
+
+    public Employer(Email email, Phone phone, Password password, TIN tin){
+        validateData(email,phone,password,tin);
+        jobs = new ArrayList<>();
+    }
+
+    private void validateData(Email email, Phone phone, Password password, TIN tin){
+        if(email == null){
+            throw new NullPointerException("Email cannot be null.");
+        }
+        if(phone == null){
+            throw new NullPointerException("Phone cannot be null.");
+        }
+        if(password == null){
+            throw new NullPointerException("Password cannot be null.");
+        }
+        if(tin == null){
+            throw new NullPointerException("TIN cannot be null");
+        }
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.tin = tin;
+    }
+
+    private void validateObject(Object obj){
+        if(obj == null){
+            throw new NullPointerException("Object cannot be null");
+        }
+    }
+    public void setEmail(Email email){
+        validateData(email,this.phone, this.password, this.tin);
+    }
+    public void setPhone(Phone phone){
+        validateData(this.email,phone, this.password, this.tin);
+    }
+    public void setPassword(Password password){
+        validateData(this.email,this.phone, password, this.tin);
+    }
+
+    public void setTin(TIN tin){
+        validateData(this.email,this.phone, this.password, tin);
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public Password getPassword() {
+        return password;
+    }
+
+    public TIN getTin() {
+        return tin;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        validateObject(companyName);
+        this.companyName = companyName;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        validateObject(sector);
+        this.sector = sector;
+    }
+
+    public void addJob (Job job){
+        validateObject(job);
+        this.jobs.add(job);
+    }
+
+    public ArrayList<Job> getJobs(){
+        return jobs;
+    }
 }
---
+++ app/src/test/java/com/example/networkmeup/EmployerTest.java
@@ -0,0 +1,97 @@
+package com.example.networkmeup;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmployerTest {
+    private Employer employer;
+
+    @Before
+    public void setup(){
+        employer = new Employer(new Email("employ@example.com"),
+                new Phone("5658274859"), new Password("Test1234!"), new TIN("000001010"));
+    }
+
+    @Test
+    public void nullJobCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+           employer.addJob(null);
+        });
+    }
+    @Test
+    public void nullCompanyNameCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            employer.setCompanyName(null);
+        });
+    }
+    @Test
+    public void nullSectorCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            employer.setSector(null);
+        });
+    }
+    @Test
+    public void nullEmailCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            employer.setEmail(null);
+        });
+    }
+
+    @Test
+    public void nullPasswordCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            employer.setPassword(null);
+        });
+    }
+    @Test
+    public void nullPhoneCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            employer.setPhone(null);
+        });
+    }
+    @Test
+    public void nullTINCheck(){
+        Assert.assertThrows(NullPointerException.class, ()->{
+            employer.setTin(null);
+        });
+    }
+
+    @Test
+    public void validCompanyNameCheck(){
+        employer.setCompanyName("Coca Cola");
+        Assert.assertEquals("Coca Cola", employer.getCompanyName());
+    }
+    @Test
+    public void validSectorCheck(){
+        employer.setSector("Sector");
+        Assert.assertEquals("Sector", employer.getSector());
+    }
+    @Test
+    public void validEmailCheck(){
+        employer.setEmail(new Email("employerTest1@gmail.com"));
+        Assert.assertEquals("employerTest1@gmail.com", employer.getEmail().getAddress());
+    }
+
+    @Test
+    public void validPhoneCheck(){
+        employer.setPhone(new Phone("8673859245"));
+        Assert.assertEquals("8673859245", employer.getPhone().getNumber());
+    }
+
+    @Test
+    public void validPasswordCheck(){
+        employer.setPassword(new Password("TestTest124!"));
+        Assert.assertEquals("TestTest124!", employer.getPassword().getPassword());
+    }
+    @Test
+    public void validTINCheck(){
+        employer.setTin(new TIN("000001010"));
+        Assert.assertEquals("000001010", employer.getTin().getTin());
+    }
+    @Test
+    public void validJobCheck(){
+        employer.addJob(new Job("Example Job", "This is an Example Job"));
+        Assert.assertEquals("Example Job", employer.getJobs().get(0).getTitle());
+    }
+}
