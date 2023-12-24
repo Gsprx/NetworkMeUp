2e78afdfbcb6120159adeece815298369728d358
Gaspar <rigos.gasparis@gmail.com>
2023-12-24 22:58:52 +0200

    started employee search job view bundle

---
+++ app/src/main/java/com/example/networkmeup/view/SearchJobs/SearchJobsActivity.java
@@ -0,0 +1,16 @@
+package com.example.networkmeup.view.SearchJobs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.networkmeup.R;
+
+public class SearchJobsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_jobs);
+    }
+}
