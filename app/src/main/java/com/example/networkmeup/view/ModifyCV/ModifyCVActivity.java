908b9b3335e273aa561664b0fb6c9e314b3f0627
Gaspar <rigos.gasparis@gmail.com>
2023-12-24 22:58:41 +0200

    started employee modify cv view bundle

---
+++ app/src/main/java/com/example/networkmeup/view/ModifyCV/ModifyCVActivity.java
@@ -0,0 +1,16 @@
+package com.example.networkmeup.view.ModifyCV;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.networkmeup.R;
+
+public class ModifyCVActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_cv_activity);
+    }
+}
