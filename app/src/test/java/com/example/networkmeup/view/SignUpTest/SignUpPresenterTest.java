4c68019c4e9b0370e562b5d78f5668b57d188e83
apokamp <apo.kampouris.2003@gmail.com>
2023-12-26 11:03:08 +0200

    Full commentation on the following files: SignUpPresenterTester, SignUpViewStub

--- app/src/test/java/com/example/networkmeup/view/SignUpTest/SignUpPresenterTest.java
+++ app/src/test/java/com/example/networkmeup/view/SignUpTest/SignUpPresenterTest.java
@@ -9,30 +9,47 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * This class is responsible for testing the functionality of the SignUpPresenter class
+ * that manages interactions between the SignUpView and user input during sign-up processes.
+ */
 public class SignUpPresenterTest {
-    private SignUpViewStub view;
-    private SignUpPresenter presenter;
+    private SignUpViewStub view; // Instance of a stub for the SignUpView interface
+    private SignUpPresenter presenter; // Instance of the SignUpPresenter being tested
 
+    /**
+     * Method executed before each test to set up the necessary components.
+     */
     @Before
     public void setup(){
-        view = new SignUpViewStub();
-        presenter = new SignUpPresenter(view);
-        view.setPresenter(presenter);
+        view = new SignUpViewStub(); // Initializing a stub of the SignUpView
+        presenter = new SignUpPresenter(view); // Creating an instance of SignUpPresenter with the view
+        view.setPresenter(presenter); // Setting the presenter for the view
     }
 
+    /**
+     * Test method to verify the number of clicks on the Employee sign-up button.
+     */
     @Test
     public void test7EmployeeClicks(){
+        // Simulating 7 clicks on the Employee sign-up button
         for (int i = 0; i < 7 ; i++){
-            presenter.onEmployee();
+            presenter.onEmployee(); // Triggering the employee sign-up event in the presenter
         }
+        // Verifying if the view recorded 7 clicks on the Employee sign-up button
         Assert.assertEquals(7,view.getEmployeeClicks());
     }
 
+    /**
+     * Test method to verify the number of clicks on the Employer sign-up button.
+     */
     @Test
     public void test4EmployerClicks(){
+        // Simulating 4 clicks on the Employer sign-up button
         for (int i = 0; i < 4 ; i++){
-            presenter.onEmployer();
+            presenter.onEmployer(); // Triggering the employer sign-up event in the presenter
         }
+        // Verifying if the view recorded 4 clicks on the Employer sign-up button
         Assert.assertEquals(4,view.getEmployerClicks());
     }
 }
--- app/src/test/java/com/example/networkmeup/view/SignUpTest/SignUpViewStub.java
+++ app/src/test/java/com/example/networkmeup/view/SignUpTest/SignUpViewStub.java
@@ -3,33 +3,64 @@ package com.example.networkmeup.view.SignUpTest;
 import com.example.networkmeup.view.SignUp.SignUpPresenter;
 import com.example.networkmeup.view.SignUp.SignUpView;
 
+/**
+ * This class represents a stub of the SignUpView interface. It's used for testing SignUpPresenter functionality.
+ */
 public class SignUpViewStub implements SignUpView {
-    private int employeeClicks;
-    private int employerClicks;
-    private SignUpPresenter presenter;
+    private int employeeClicks; // Counter to track the number of clicks on the Employee sign-up button
+    private int employerClicks; // Counter to track the number of clicks on the Employer sign-up button
+    private SignUpPresenter presenter; // Reference to the associated SignUpPresenter
+
+    /**
+     * Default constructor for SignUpViewStub.
+     */
     public SignUpViewStub (){
     }
 
+    /**
+     * Method to simulate a click on the Employee sign-up button.
+     * It increments the employeeClicks counter.
+     */
     public void employee(){
         employeeClicks++;
     }
 
+    /**
+     * Method to simulate a click on the Employer sign-up button.
+     * It increments the employerClicks counter.
+     */
     public void employer(){
         employerClicks++;
     }
 
+    /**
+     * Getter method to retrieve the associated SignUpPresenter.
+     * @return The SignUpPresenter instance associated with this view.
+     */
     public SignUpPresenter getPresenter() {
         return presenter;
     }
 
+    /**
+     * Setter method to set the SignUpPresenter for this view.
+     * @param presenter The SignUpPresenter instance to be set for this view.
+     */
     public void setPresenter(SignUpPresenter presenter) {
         this.presenter = presenter;
     }
 
+    /**
+     * Getter method to retrieve the number of clicks on the Employee sign-up button.
+     * @return The count of clicks on the Employee sign-up button.
+     */
     public int getEmployeeClicks() {
         return employeeClicks;
     }
 
+    /**
+     * Getter method to retrieve the number of clicks on the Employer sign-up button.
+     * @return The count of clicks on the Employer sign-up button.
+     */
     public int getEmployerClicks() {
         return employerClicks;
     }
