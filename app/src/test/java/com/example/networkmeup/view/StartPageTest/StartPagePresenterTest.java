6df570aeecb447fba60a3f354f2cbedc7bd1eba0
apokamp <apo.kampouris.2003@gmail.com>
2023-12-26 11:09:39 +0200

    Full commentation on the following files: StartPagePresenterTest, StartPageViewStub

--- app/src/test/java/com/example/networkmeup/view/StartPageTest/StartPagePresenterTest.java
+++ app/src/test/java/com/example/networkmeup/view/StartPageTest/StartPagePresenterTest.java
@@ -6,30 +6,46 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * This class is responsible for testing the functionality of the StartPagePresenter.
+ * It verifies the behavior of the presenter by simulating user interactions with the associated view.
+ */
 public class StartPagePresenterTest {
-    private StartPagePresenter presenter;
-    private StartPageViewStub view;
+    private StartPagePresenter presenter; // The presenter being tested
+    private StartPageViewStub view; // Stub of the StartPageView for testing
 
+    /**
+     * Method to set up the initial conditions for each test case.
+     * It creates a new StartPageViewStub and associates it with the StartPagePresenter.
+     */
     @Before
     public void setup(){
-        view = new StartPageViewStub();
-        presenter = new StartPagePresenter(view);
-        view.setPresenter(presenter);
+        view = new StartPageViewStub(); // Creating a stub for the view
+        presenter = new StartPagePresenter(view); // Creating a presenter with the stubbed view
+        view.setPresenter(presenter); // Setting the presenter for the view
     }
 
+    /**
+     * Test method to verify the functionality of the onLogin method in the presenter.
+     * It simulates 10 login button clicks and checks if the presenter records the clicks accurately.
+     */
     @Test
     public void test10LoginClicks(){
         for (int i = 0; i < 10 ; i++){
-            presenter.onLogin();
+            presenter.onLogin(); // Simulating a click on the login button
         }
-        Assert.assertEquals(10,view.getLoginClicks());
+        Assert.assertEquals(10,view.getLoginClicks()); // Verifying the count of login clicks recorded by the view
     }
 
+    /**
+     * Test method to verify the functionality of the onSignUp method in the presenter.
+     * It simulates 13 sign-up button clicks and checks if the presenter records the clicks accurately.
+     */
     @Test
     public void test13SignupClicks(){
         for (int i = 0; i < 13 ; i++){
-            presenter.onSignUp();
+            presenter.onSignUp(); // Simulating a click on the sign-up button
         }
-        Assert.assertEquals(13,view.getSignupClicks());
+        Assert.assertEquals(13,view.getSignupClicks()); // Verifying the count of sign-up clicks recorded by the view
     }
 }
--- app/src/test/java/com/example/networkmeup/view/StartPageTest/StartPageViewStub.java
+++ app/src/test/java/com/example/networkmeup/view/StartPageTest/StartPageViewStub.java
@@ -3,31 +3,59 @@ package com.example.networkmeup.view.StartPageTest;
 import com.example.networkmeup.view.StartPage.StartPagePresenter;
 import com.example.networkmeup.view.StartPage.StartPageView;
 
+/**
+ * This class acts as a stub implementation of the StartPageView interface for testing purposes.
+ * It mimics the behavior of the actual view to facilitate testing of the StartPagePresenter.
+ */
 public class StartPageViewStub implements StartPageView {
-    private int loginClicks;
-    private int signupClicks;
-    private StartPagePresenter presenter;
+    private int loginClicks; // Counter to track the number of login button clicks
+    private int signupClicks; // Counter to track the number of signup button clicks
+    private StartPagePresenter presenter; // Reference to the associated presenter
 
+    /**
+     * Sets the presenter for this view stub.
+     * @param presenter The presenter instance to be associated with this stub.
+     */
     public void setPresenter(StartPagePresenter presenter) {
         this.presenter = presenter;
     }
 
+    /**
+     * Retrieves the presenter associated with this view stub.
+     * @return The presenter instance associated with this stub.
+     */
     public StartPagePresenter getPresenter() {
         return presenter;
     }
 
+    /**
+     * Simulates a user action of clicking the login button.
+     * Increases the count of login button clicks recorded by this view stub.
+     */
     public void login(){
         loginClicks++;
     }
 
+    /**
+     * Simulates a user action of clicking the signup button.
+     * Increases the count of signup button clicks recorded by this view stub.
+     */
     public void signup(){
         signupClicks++;
     }
 
+    /**
+     * Retrieves the number of recorded login button clicks.
+     * @return The count of login button clicks registered by this view stub.
+     */
     public int getLoginClicks() {
         return loginClicks;
     }
 
+    /**
+     * Retrieves the number of recorded signup button clicks.
+     * @return The count of signup button clicks registered by this view stub.
+     */
     public int getSignupClicks() {
         return signupClicks;
     }
