baa46c708f6a978ac8f289fec6518378735b644c
apokamp <apo.kampouris.2003@gmail.com>
2023-12-26 10:55:36 +0200

    Full comentation on the following files: SignUpEmployerPresenterTest, SignUpEmployerViewStub

--- app/src/test/java/com/example/networkmeup/view/SignUpEmployerTest/SignUpEmployerPresenterTest.java
+++ app/src/test/java/com/example/networkmeup/view/SignUpEmployerTest/SignUpEmployerPresenterTest.java
@@ -19,6 +19,7 @@ public class SignUpEmployerPresenterTest {
 
     @Before
     public void setup(){
+        // Setting up the data initializer and view stub before each test case
         dataInitializer = new MemoryInitializer();
         dataInitializer.prepareData();
         view = new SignUpEmployerViewStub();
@@ -26,101 +27,114 @@ public class SignUpEmployerPresenterTest {
 
     @Test
     public void testCreateNewAccount(){
-        //checking all possible outcomes for employer signup
+        // Testing various scenarios for employer sign-up
 
         presenter = new SignUpEmployerPresenter(view, new EmployerDAOMemory());
 
-        //no input, attempt to create account
-
+        // Attempt to create an account without any input
         presenter.onCreate();
-        //the last update to the showErrorMessage in our stub, is the TIN field, we can only check this at this instance
+        // The last update to the showErrorMessage in our stub, is the TIN field, we can only check this at this instance
+        // Expecting an error message for TIN field being empty
         Assert.assertEquals("Error!", view.getShowErrorMessageTitle());
         Assert.assertEquals("TIN cannot be empty.", view.getShowErrorMessageMsg());
 
-        //add a wrong tin
+        // Adding an incorrect TIN format
         view.setTinField("Company3414");
         presenter.onCreate();
+        // Expecting an error message for an invalid TIN format
         Assert.assertEquals("Error!", view.getShowErrorMessageTitle());
         Assert.assertEquals("Invalid TIN.", view.getShowErrorMessageMsg());
 
-        //add a right tin
+        // Adding a correct TIN but no password
         view.setTinField("341423139");
         presenter.onCreate();
+        // Expecting an error message for an empty password
         Assert.assertEquals("Error!", view.getShowErrorMessageTitle());
         Assert.assertEquals("Password cannot be empty.", view.getShowErrorMessageMsg());
 
-        //add a weak password
+        // Adding a weak password
         view.setPasswordField("Password123");
         presenter.onCreate();
+        // Expecting an error message for a weak password
         Assert.assertEquals("Error!", view.getShowErrorMessageTitle());
         Assert.assertEquals("Password is not strong enough.", view.getShowErrorMessageMsg());
 
-        //add a strong password
+        // Adding a strong password but no phone number
         view.setPasswordField("Password!234");
         presenter.onCreate();
-        //password field no longer throws an exception -> theres no error message update
-        //last update to showErrorMessage happens on the phone field
-        //phone number and email are empty
+        // Expecting an error message for an empty phone number
+        // Password field no longer throws an exception -> theres no error message update
+        // Last update to showErrorMessage happens on the phone field
+        // Phone number and email are empty
         Assert.assertEquals("Error!", view.getShowErrorMessageTitle());
         Assert.assertEquals("Phone number cannot be empty.", view.getShowErrorMessageMsg());
 
-        //add an invalid phone number
+        // Adding an invalid phone number
         view.setPhoneField("124512347");
         presenter.onCreate();
+        // Expecting an error message for an invalid phone number
         Assert.assertEquals("Error!", view.getShowErrorMessageTitle());
         Assert.assertEquals("Invalid phone number.", view.getShowErrorMessageMsg());
 
-        //add a valid phone number
+        // Adding a valid phone number but no email
         view.setPhoneField("8674829405");
         presenter.onCreate();
-        //password and phone field no longer throw exceptions -> theres no error message update
-        //last update to showErrorMessage happens in the email field
-        //email field is currently empty
+        // Expecting an error message for an empty email
+        // Password and phone field no longer throw exceptions -> theres no error message update
+        // Last update to showErrorMessage happens in the email field
+        // Email field is currently empty
         Assert.assertEquals("Error!", view.getShowErrorMessageTitle());
         Assert.assertEquals("Email address cannot be empty.", view.getShowErrorMessageMsg());
 
-        //add an invalid email address
+        // Adding an invalid email address
         view.setEmailField("@email.com");
         presenter.onCreate();
+        // Expecting an error message for an invalid email address
         Assert.assertEquals("Error!", view.getShowErrorMessageTitle());
         Assert.assertEquals("Invalid email address.", view.getShowErrorMessageMsg());
 
-        //add a valid email address
+        // Adding a valid email address, expecting successful account creation
         view.setEmailField("example@email.com");
         presenter.onCreate();
+        // Expecting a success message for account creation
         Assert.assertEquals("Your NetworkMeUp Employer account was created!", view.getSuccessfullyFinishActivityMessage());
     }
     @Test
     public void testCreateExistingAccount(){
 
-        presenter = new SignUpEmployerPresenter(view, new EmployerDAOMemory());
-        //check when creating an account with any existing details in the system
-        //password does not need to be unique
+        // Testing scenario when creating an account with existing details in the system
 
-        //unique phone but existing email
+        presenter = new SignUpEmployerPresenter(view, new EmployerDAOMemory());
+        // Check when creating an account with any existing details in the system
+        // Password does not need to be unique
+
+        // Creating an account with a unique phone number but an existing email
         view.setTinField("341423139");
         view.setEmailField("b.be@northfreedom.com");
         view.setPhoneField("8795175834");
         view.setPasswordField("Test1234!");
         presenter.onCreate();
+        // Expecting an error message as an employer account already exists with the same email or phone number or TIN
         Assert.assertEquals("Account Creation Error", view.getShowErrorMessageTitle());
         Assert.assertEquals("An employer account already exists with the same email or phone number or TIN!", view.getShowErrorMessageMsg());
 
-        //unique email but existing phone
+        // Creating an account with a unique email but an existing phone number
         view.setTinField("341423139");
         view.setEmailField("random.email@yahoo.com");
         view.setPhoneField("5693311692");
         view.setPasswordField("Test1234!");
         presenter.onCreate();
+        // Expecting an error message as an employer account already exists with the same email or phone number or TIN
         Assert.assertEquals("Account Creation Error", view.getShowErrorMessageTitle());
         Assert.assertEquals("An employer account already exists with the same email or phone number or TIN!", view.getShowErrorMessageMsg());
 
-        //existing email and phone
+        // Creating an account with existing email and phone number
         view.setTinField("341423139");
         view.setEmailField("Company.188@gmail.com");
         view.setPhoneField("6953619405");
         view.setPasswordField("Test1234!");
         presenter.onCreate();
+        // Expecting an error message as an employer account already exists with the same email or phone number or TIN
         Assert.assertEquals("Account Creation Error", view.getShowErrorMessageTitle());
         Assert.assertEquals("An employer account already exists with the same email or phone number or TIN!", view.getShowErrorMessageMsg());
 
--- app/src/test/java/com/example/networkmeup/view/SignUpEmployerTest/SignUpEmployerViewStub.java
+++ app/src/test/java/com/example/networkmeup/view/SignUpEmployerTest/SignUpEmployerViewStub.java
@@ -10,85 +10,156 @@ import com.example.networkmeup.view.SignUpEmployerTest.SignUpEmployerViewStub;
 import com.example.networkmeup.view.SignUpEmployer.SignUpEmployerPresenter;
 import com.example.networkmeup.view.SignUpEmployer.SignUpEmployerView;
 
+/**
+ * This class represents a stub of the View component for the Employer Sign-Up process used in testing.
+ * It implements the SignUpEmployerView interface to simulate user interface actions and display feedback.
+ */
 public class SignUpEmployerViewStub implements SignUpEmployerView {
 
-    private SignUpEmployerPresenter presenter;
-    private String emailField;
-    private String phoneField;
-    private String passwordField;
+    private SignUpEmployerPresenter presenter; // Presenter instance associated with this view stub
+    private String emailField; // Simulated input field for email address
+    private String phoneField; // Simulated input field for phone number
+    private String passwordField; // Simulated input field for password
 
-    private String tinField;
-    private String  showErrorMessageMsg;
-    private String showErrorMessageTitle;
-    private String  successfullyFinishActivityMessage;
+    private String tinField; // Simulated input field for Tax Identification Number (TIN)
+    private String  showErrorMessageMsg; // Message to display for an error scenario
+    private String showErrorMessageTitle; // Title to display for an error scenario
+    private String  successfullyFinishActivityMessage; // Message to display for a successful activity completion
 
+    /**
+     * Default constructor to initialize the SignUpEmployerViewStub.
+     */
     public SignUpEmployerViewStub(){
     }
 
+    /**
+     * Sets the presenter associated with this view stub.
+     * @param presenter SignUpEmployerPresenter instance
+     */
     public void setPresenter(SignUpEmployerPresenter presenter){
         this.presenter = presenter;
     }
 
+    /**
+     * Retrieves the presenter associated with this view stub.
+     * @return SignUpEmployerPresenter instance
+     */
     public SignUpEmployerPresenter getPresenter(){
         return this.presenter;
     }
 
+    /**
+     * Retrieves the email information entered.
+     * @return Email object representing the entered email
+     * @throws RuntimeException when an invalid email format is detected
+     */
     @Override
     public Email getEmail() throws RuntimeException {
         return new Email(emailField);
     }
 
+    /**
+     * Retrieves the password information entered.
+     * @return Password object representing the entered password
+     * @throws RuntimeException when an invalid password format is detected
+     */
     @Override
     public Password getPassword() throws RuntimeException {
         return new Password(passwordField);
     }
 
+    /**
+     * Retrieves the phone number information entered.
+     * @return Phone object representing the entered phone number
+     * @throws RuntimeException when an invalid phone number format is detected
+     */
     @Override
     public Phone getPhone() throws RuntimeException {
         return new Phone(phoneField);
     }
 
+    /**
+     * Retrieves the TIN information entered.
+     * @return TIN object representing the entered Tax Identification Number
+     * @throws RuntimeException when an invalid TIN format is detected
+     */
     @Override
     public TIN getTIN() throws RuntimeException {
         return new TIN(tinField);
     }
 
+    /**
+     * Sets the value for the email field.
+     * @param emailField Simulated input for email address
+     */
     public void setEmailField(String emailField) {
         this.emailField = emailField;
     }
 
+    /**
+     * Sets the value for the phone field.
+     * @param phoneField Simulated input for phone number
+     */
     public void setPhoneField(String phoneField) {
         this.phoneField = phoneField;
     }
 
+    /**
+     * Sets the value for the password field.
+     * @param passwordField Simulated input for password
+     */
     public void setPasswordField(String passwordField) {
         this.passwordField = passwordField;
     }
 
+    /**
+     * Sets the value for the TIN field.
+     * @param tinField Simulated input for Tax Identification Number
+     */
     public void setTinField(String TinField) {
         this.tinField = TinField;
     }
 
-
+    /**
+     * Displays an error message on the simulated user interface.
+     * @param title Title of the error message
+     * @param message Content of the error message
+     */
     @Override
     public void showErrorMessage(String title, String message) {
         this.showErrorMessageTitle =title;
         this.showErrorMessageMsg = message;
     }
 
+    /**
+     * Displays a success message on the simulated user interface.
+     * @param message Content of the success message
+     */
     @Override
     public void successfullyFinishActivity(String message) {
         this.successfullyFinishActivityMessage = message;
     }
 
+    /**
+     * Retrieves the error message to be displayed.
+     * @return Error message content
+     */
     public String getShowErrorMessageMsg() {
         return showErrorMessageMsg;
     }
 
+    /**
+     * Retrieves the error message title to be displayed.
+     * @return Error message title
+     */
     public String getShowErrorMessageTitle() {
         return showErrorMessageTitle;
     }
 
+    /**
+     * Retrieves the success message to be displayed upon completion.
+     * @return Success message content
+     */
     public String getSuccessfullyFinishActivityMessage() {
         return successfullyFinishActivityMessage;
     }
