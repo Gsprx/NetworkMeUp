20d23e153e75c3c11a796afbabf9d108ba3bb86a
apokamp <apo.kampouris.2003@gmail.com>
2023-12-26 10:26:49 +0200

    Full commentation on the following files: LoginPresenterTest, LoginViewStub

--- app/src/test/java/com/example/networkmeup/view/LoginTest/LoginPresenterTest.java
+++ app/src/test/java/com/example/networkmeup/view/LoginTest/LoginPresenterTest.java
@@ -9,12 +9,20 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * This class contains test cases for the LoginPresenter class.
+ * It tests the functionality of the LoginPresenter regarding user login actions.
+ */
 public class LoginPresenterTest {
 
     private LoginViewStub view;
 
     private LoginPresenter presenter;
 
+    /**
+     * Initializes necessary objects before each test case.
+     * Sets up the LoginPresenter with a stub view and associates the presenter with the view.
+     */
     @Before
     public void setup(){
         view = new LoginViewStub();
@@ -22,6 +30,9 @@ public class LoginPresenterTest {
         view.setPresenter(presenter);
     }
 
+    /**
+     * Tests if the onLoginEmployee method in the presenter correctly increments the employee login clicks.
+     */
     @Test
     public void test9EmployeeClicks(){
         for (int i = 0; i < 9 ; i++){
@@ -30,6 +41,9 @@ public class LoginPresenterTest {
         Assert.assertEquals(9,view.getEmployeeClicks());
     }
 
+    /**
+     * Tests if the onLoginEmployer method in the presenter correctly increments the employer login clicks.
+     */
     @Test
     public void test7EmployerClicks(){
         for (int i = 0; i <7 ; i++){
--- app/src/test/java/com/example/networkmeup/view/LoginTest/LoginViewStub.java
+++ app/src/test/java/com/example/networkmeup/view/LoginTest/LoginViewStub.java
@@ -4,35 +4,69 @@ import com.example.networkmeup.view.Login.LoginPresenter;
 import com.example.networkmeup.view.Login.LoginView;
 import com.example.networkmeup.view.SignUp.SignUpPresenter;
 
+/**
+ * This class acts as a stub implementation of the LoginView interface for testing purposes.
+ * It simulates a view and keeps track of employee and employer login actions.
+ */
 public class LoginViewStub implements LoginView {
 
-    private int employeeclicks;
+    private int employeeclicks; // Counter for employee login clicks
 
-    private int employerclicks;
+    private int employerclicks; // Counter for employer login clicks
 
-    private LoginPresenter presenter;
+    private LoginPresenter presenter; // Reference to the LoginPresenter
 
+    /**
+     * Default constructor.
+     */
     public LoginViewStub(){}
 
+    /**
+     * Simulates an employee login action by incrementing the employee clicks counter.
+     */
     public void EmployeeLogin(){
         employeeclicks++;
     }
 
+    /**
+     * Simulates an employer login action by incrementing the employer clicks counter.
+     */
     public void EmployerLogin(){
         employerclicks++;
     }
+
+    /**
+     * Getter method for retrieving the presenter associated with this view stub.
+     *
+     * @return The LoginPresenter associated with this view stub.
+     */
     public LoginPresenter getPresenter() {
         return presenter;
     }
 
+    /**
+     * Setter method for setting the presenter associated with this view stub.
+     *
+     * @param presenter The LoginPresenter to associate with this view stub.
+     */
     public void setPresenter(LoginPresenter presenter) {
         this.presenter = presenter;
     }
 
+    /**
+     * Getter method for retrieving the number of employee login clicks.
+     *
+     * @return The count of employee login clicks.
+     */
     public int getEmployeeClicks() {
         return employeeclicks;
     }
 
+    /**
+     * Getter method for retrieving the number of employer login clicks.
+     *
+     * @return The count of employer login clicks.
+     */
     public int getEmployerClicks() {
         return employerclicks;
     }
