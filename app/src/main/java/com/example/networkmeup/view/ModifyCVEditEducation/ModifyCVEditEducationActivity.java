7f85f3e63cebe50ac6d738c3e2be5a0d71015ca1
Gaspar <rigos.gasparis@gmail.com>
2023-12-29 23:46:09 +0200

    created activity files and folders for each activity related to modify CV

---
+++ app/src/main/java/com/example/networkmeup/view/ModifyCVEditEducation/ModifyCVEditEducationActivity.java
@@ -0,0 +1,16 @@
+package com.example.networkmeup.view.ModifyCVEditEducation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.networkmeup.R;
+
+public class ModifyCVEditEducationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_cv_edit_education);
+    }
+}
---
+++ app/src/main/java/com/example/networkmeup/view/ModifyCVEditWorkExperience/ModifyCVEditWorkExperienceActivity.java
@@ -0,0 +1,16 @@
+package com.example.networkmeup.view.ModifyCVEditWorkExperience;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.networkmeup.R;
+
+public class ModifyCVEditWorkExperienceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_cv_edit_work_experience);
+    }
+}
