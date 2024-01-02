3f079174e1cf4e7ea59158eb4aed286fc4e0e4c9
Gaspar <rigos.gasparis@gmail.com>
2024-01-02 16:13:59 +0200

    updates files

---
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/ChangeJobDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.networkmeup.R;
+
+public class ChangeJobDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_job_details);
+    }
+}
