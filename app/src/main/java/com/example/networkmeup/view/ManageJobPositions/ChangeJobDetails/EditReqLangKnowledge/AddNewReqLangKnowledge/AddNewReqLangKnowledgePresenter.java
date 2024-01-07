e8444b0f4987faf31e5347bf376c2a7af359967e
apokamp <apo.kampouris.2003@gmail.com>
2024-01-07 10:24:14 +0200

    Small change on the AddNewReqKnowledgeView

--- app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/AddNewReqLangKnowledge/AddNewReqLangKnowledgePresenter.java
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/AddNewReqLangKnowledge/AddNewReqLangKnowledgePresenter.java
@@ -1,4 +1,28 @@
 package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.AddNewReqLangKnowledge;
 
+import com.example.networkmeup.daoMemory.ExpertiseAreaDAOMemory;
+import com.example.networkmeup.domain.Education;
+import com.example.networkmeup.domain.ExpertiseArea;
+import com.example.networkmeup.domain.Job;
+import com.example.networkmeup.domain.LevelOfStudies;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqEducation.AddNewReqEducation.AddNewReqEducationView;
+
 public class AddNewReqLangKnowledgePresenter {
+    private AddNewReqLangKnowledgeView view;
+    private String userToken;
+    private Job job;
+
+    public AddNewReqLangKnowledgePresenter(AddNewReqLangKnowledgeView view, String userToken, Job job) {
+        this.view = view;
+        this.userToken = userToken;
+        this.job = job;
+    }
+
+    public void onAdd() {
+        ExpertiseArea newExpArea = new ExpertiseAreaDAOMemory().getAll().get(view.getExpertiseArea());
+        LevelOfStudies newLevelOfStudies = LevelOfStudies.values()[view.getLevelOfStudies()];
+        job.addReqEducation(new Education(view.getDescription(), newExpArea, newLevelOfStudies));
+
+        view.successfulAdd("Required Education was successfully created!", userToken, job);
+    }
 }
--- app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/AddNewReqLangKnowledge/AddNewReqLangKnowledgeView.java
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/AddNewReqLangKnowledge/AddNewReqLangKnowledgeView.java
@@ -1,9 +1,10 @@
 package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.AddNewReqLangKnowledge;
 
+import com.example.networkmeup.domain.Job;
 import com.example.networkmeup.domain.LevelOfKnowledge;
 
 public interface AddNewReqLangKnowledgeView {
-        void successfulAdd(String message, String userToken);
+        void successfulAdd(String message, String userToken, Job job);
         String getDescription();
         public int getLanguage();
         public LevelOfKnowledge getLevelOfKnowledge();
