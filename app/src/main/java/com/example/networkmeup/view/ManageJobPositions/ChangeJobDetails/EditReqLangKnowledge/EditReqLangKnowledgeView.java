ae5a92e12009ea41daa4f92c1ba012fb499da2c6
apokamp <apo.kampouris.2003@gmail.com>
2024-01-07 11:53:14 +0200

    EditReqLangKnowledgeView

--- app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/EditReqLangKnowledgeView.java
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/EditReqLangKnowledgeView.java
@@ -1,4 +1,8 @@
 package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge;
 
+import com.example.networkmeup.domain.Job;
+
 public interface EditReqLangKnowledgeView {
+    public void changeLanguageKnowledgeDetails(String userToken, int position, Job job);
+    public void addNewLanguageKnowledge(String userToken, Job job);
 }
