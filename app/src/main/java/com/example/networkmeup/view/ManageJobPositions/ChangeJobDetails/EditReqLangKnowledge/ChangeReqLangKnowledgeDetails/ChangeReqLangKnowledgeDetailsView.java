e264144a62d759b51b181c71851655cc9921e5d6
apokamp <apo.kampouris.2003@gmail.com>
2024-01-07 11:00:40 +0200

    ChangeNewLangKnowledgeDetailsView

--- app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/ChangeReqLangKnowledgeDetails/ChangeReqLangKnowledgeDetailsView.java
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/ChangeReqLangKnowledgeDetails/ChangeReqLangKnowledgeDetailsView.java
@@ -1,4 +1,12 @@
 package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.ChangeReqLangKnowledgeDetails;
 
+import com.example.networkmeup.domain.Job;
+import com.example.networkmeup.domain.LevelOfKnowledge;
+
 public interface ChangeReqLangKnowledgeDetailsView {
+    public String getDescription();
+    public int getLanguage();
+    public LevelOfKnowledge getLevelOfKnowledge();
+    void successfulDelete(String message, String userToken, Job job);
+    void successfulSave(String message, String userToken, Job job);
 }
