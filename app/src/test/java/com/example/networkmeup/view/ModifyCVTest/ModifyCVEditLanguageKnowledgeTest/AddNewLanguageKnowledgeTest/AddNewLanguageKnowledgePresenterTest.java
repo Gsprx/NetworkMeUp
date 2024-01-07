dfcf017ff0ee08ca2d4011f0934481cd8197cbe9
apokamp <apo.kampouris.2003@gmail.com>
2024-01-07 22:39:26 +0200

    AddNewLanguageKnowledge fully covered presenter test (100%)

--- app/src/test/java/com/example/networkmeup/view/ModifyCVTest/ModifyCVEditLanguageKnowledgeTest/AddNewLanguageKnowledgeTest/AddNewLanguageKnowledgePresenterTest.java
+++ app/src/test/java/com/example/networkmeup/view/ModifyCVTest/ModifyCVEditLanguageKnowledgeTest/AddNewLanguageKnowledgeTest/AddNewLanguageKnowledgePresenterTest.java
@@ -1,15 +1,19 @@
 package com.example.networkmeup.view.ModifyCVTest.ModifyCVEditLanguageKnowledgeTest.AddNewLanguageKnowledgeTest;
 
 import com.example.networkmeup.dao.EmployeeDAO;
+import com.example.networkmeup.dao.Initializer;
 import com.example.networkmeup.dao.LanguageDAO;
 import com.example.networkmeup.daoMemory.EmployeeDAOMemory;
 import com.example.networkmeup.daoMemory.LanguageDAOMemory;
+import com.example.networkmeup.daoMemory.MemoryInitializer;
 import com.example.networkmeup.domain.Email;
 import com.example.networkmeup.domain.Employee;
 import com.example.networkmeup.domain.Language;
 import com.example.networkmeup.domain.LevelOfKnowledge;
 import com.example.networkmeup.domain.LanguageKnowledge;
+import com.example.networkmeup.view.ModifyCV.ModifyCVEditEducation.AddNewEducation.AddNewEducationPresenter;
 import com.example.networkmeup.view.ModifyCV.ModifyCVEditLanguageKnowledge.AddNewLanguageKnowledge.AddNewLanguageKnowledgePresenter;
+import com.example.networkmeup.view.ModifyCVTest.ModifyCVEditEducationTest.AddNewEducationTest.AddNewEducationViewStub;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,41 +25,42 @@ import java.util.List;
 public class AddNewLanguageKnowledgePresenterTest {
     private AddNewLanguageKnowledgePresenter presenter;
     private AddNewLanguageKnowledgeViewStub stub;
+    private Initializer initializer;
 
     @Before
-    public void setup() {
+    public void setup(){
         stub = new AddNewLanguageKnowledgeViewStub();
         presenter = new AddNewLanguageKnowledgePresenter(stub, "john.Brown12@gmail.com");
+        initializer = new MemoryInitializer();
+        initializer.prepareData();
     }
 
     @Test
-    public void checkAddLanguageKnowledge() {
-        stub.setDescription("Description Test");
-        stub.setLanguage(1); // Assuming language at position 1 in the list
-        stub.setLevelOfKnowledge(LevelOfKnowledge.Amateur); // Assuming level of knowledge at position 2 in the enum
+    public void checkAddEducation(){
+        //attributes set to simulated activity widgets
+        stub.setDescription("Description Test");//Description text
+        stub.setLanguage(0); //English
+        stub.setLevelOfKnowledge(LevelOfKnowledge.Amateur); //Amateur
 
-        presenter.onAdd();
+        presenter.onAdd(); //new language knowledge to be added
 
-        Assert.assertEquals("LanguageKnowledge has been created successfully!", stub.getSuccessMessage());
-        Assert.assertEquals("john.Brown12@gmail.com", stub.getUserToken());
+        //testing view side data
+        Assert.assertEquals("Language Knowledge has been created successfully!",stub.getLastDialogMessage());
+        Assert.assertEquals("john.Brown12@gmail.com", stub.getLastTokenPassed());
 
+        //testing database side data
         EmployeeDAO employeeDAO = new EmployeeDAOMemory();
         Employee curEmployee = employeeDAO.getByEmail(new Email("john.Brown12@gmail.com"));
 
-        Assert.assertEquals(1, curEmployee.getCV().getLanguageKnowledge().size());
+        //check if education was actually added
+        Assert.assertEquals(3, curEmployee.getCV().getEducation().size());
 
-        LanguageDAO languageDAO = new LanguageDAOMemory();
-        Language language = languageDAO.getAll().get(stub.getLanguage());
+        //check if education has the right attributes
 
-        LevelOfKnowledge levelOfKnowledge = LevelOfKnowledge.values()[stub.getLevelOfKnowledge().ordinal()];
+        Assert.assertEquals("Description Test", curEmployee.getCV().getEducation().get(2).getDescription());
+        Assert.assertEquals("Agriculture", curEmployee.getCV().getEducation().get(2).getExpArea().getArea());
+        Assert.assertEquals("Junior_High_School", curEmployee.getCV().getEducation().get(2).getLvlOfStudies().toString());
 
-        LanguageKnowledge addedLanguageKnowledge = new LanguageKnowledge(
-                stub.getDescription(),
-                language,
-                levelOfKnowledge
-        );
-
-        Assert.assertEquals(addedLanguageKnowledge, curEmployee.getCV().getLanguageKnowledge().get(0));
     }
 }
 
