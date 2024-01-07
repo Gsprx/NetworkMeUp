33da7f61d7b2fc6422f6a908562d945e46ffa992
apokamp <apo.kampouris.2003@gmail.com>
2024-01-07 10:44:13 +0200

    AddNewReqLangKnowledgeActivity

--- app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/AddNewReqLangKnowledge/AddNewReqLangKnowledgeActivity.java
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/AddNewReqLangKnowledge/AddNewReqLangKnowledgeActivity.java
@@ -1,16 +1,181 @@
 package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.AddNewReqLangKnowledge;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.networkmeup.R;
+import com.example.networkmeup.dao.ExpertiseAreaDAO;
+import com.example.networkmeup.dao.LanguageDAO;
+import com.example.networkmeup.daoMemory.ExpertiseAreaDAOMemory;
+import com.example.networkmeup.daoMemory.LanguageDAOMemory;
+import com.example.networkmeup.domain.ExpertiseArea;
+import com.example.networkmeup.domain.Job;
+import com.example.networkmeup.domain.Language;
+import com.example.networkmeup.domain.LevelOfKnowledge;
+import com.example.networkmeup.domain.LevelOfStudies;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.ChangeJobDetailsActivity;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqEducation.AddNewReqEducation.AddNewReqEducationActivity;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqEducation.AddNewReqEducation.AddNewReqEducationPresenter;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqEducation.EditReqEducationActivity;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.EditReqLangKnowledgeActivity;
 
-public class AddNewReqLangKnowledgeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class AddNewReqLangKnowledgeActivity extends AppCompatActivity implements AddNewReqLangKnowledgeView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_req_lang_knowledge);
+        Bundle extras = getIntent().getExtras();
+
+        final String userEmail;
+        final Job currJob;
+        if(extras!=null){
+            //obtain user token
+            userEmail = extras.getString("token");
+            currJob = (Job)extras.getSerializable("job");
+        }
+        else{
+            userEmail = null;
+            currJob = null;
+        }
+
+        final AddNewReqLangKnowledgePresenter presenter = new AddNewReqLangKnowledgePresenter(this, userEmail, currJob);
+
+        //create spinner declarations
+        Spinner languageSpinner = findViewById(R.id.spinnerAddNewReqLangKnowledgeSelectLang);
+        Spinner lvlOfKnowledgeSpinner = findViewById(R.id.spinnerAddNewReqLangKnowledgeSelectLevelOfKnowledge);
+
+        ArrayList<String> levelsOfKnowledge = new ArrayList<>();
+        ArrayList<String> languages = new ArrayList<>();
+
+        //create spinner list for levels of knowledge
+        for(int i = 0; i< LevelOfKnowledge.values().length; i++){
+            levelsOfKnowledge.add(LevelOfKnowledge.values()[i].toString());
+        }
+
+        //create spinner list for languages
+        LanguageDAO languageDAO = new LanguageDAOMemory();
+        ArrayList<Language> Languages = languageDAO.getAll();
+        for(Language language : Languages){
+            Languages.add(language);
+        }
+
+        //pass adapter to spinners and define behavior
+        ArrayAdapter<String> levelsOfKnowledgeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, levelsOfKnowledge);
+        lvlOfKnowledgeSpinner.setAdapter(levelsOfKnowledgeAdapter);
+        lvlOfKnowledgeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedLvlOfKnowledge = levelsOfKnowledge.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //do nothing
+            }
+        });
+
+        ArrayAdapter<String> languagesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, languages);
+        languageSpinner.setAdapter(languagesAdapter);
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedLanguage = languages.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //do nothing
+            }
+        });
+
+
+
+        //when add button is pressed
+        findViewById(R.id.btnAddNewReqLangknowledge).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        presenter.onAdd();
+                    }
+                }
+        );
+        // when back button is pressed
+        findViewById(R.id.backbuttonAddNewReqLangKnowledge).setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        Intent intent = new Intent(AddNewReqLangKnowledgeActivity.this, EditReqLangKnowledgeActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void successfulAdd(String message, String userToken, Job job){
+        new AlertDialog.Builder(AddNewReqLangKnowledgeActivity.this)
+                .setCancelable(false)
+                .setTitle("Creation Completed!")
+                .setMessage(message)
+
+                //return back to modify job view when pressed
+                .setPositiveButton("Return to Change Job Details",
+                        new DialogInterface.OnClickListener(){
+
+                            public void onClick (DialogInterface dialog,int id) {
+
+                                Intent intent = new Intent(AddNewReqLangKnowledgeActivity.this, ChangeJobDetailsActivity.class);
+                                intent.putExtra("token", userToken);
+                                intent.putExtra("job", job);
+                                startActivity(intent);
+                            }}).create().show();
+    }
+    @Override
+    public String getDescription() {
+        return ((EditText)findViewById(R.id.editTextAddNewReqLangKnowledgeDescription)).getText().toString().trim();
+    }
+
+    @Override
+    public int getLanguage() {
+        //get position in respective list
+        return ((Spinner)findViewById(R.id.spinnerAddNewReqLangKnowledgeSelectLang)).getSelectedItemPosition();
+    }
+
+    @Override
+    public LevelOfKnowledge getLevelOfKnowledge() {
+
+        Spinner spinner = ((Spinner)findViewById(R.id.spinnerAddNewReqLangKnowledgeSelectLevelOfKnowledge));
+        int selectedPosition = spinner.getSelectedItemPosition();
+        // Determine the LevelOfKnowledge based on the selected position
+        switch (selectedPosition) {
+            case 0:
+                return LevelOfKnowledge.Amateur;
+            case 1:
+                return LevelOfKnowledge.Lower;
+            case 2:
+                return LevelOfKnowledge.Advanced;
+            case 3:
+                return LevelOfKnowledge.Proficiency;
+            case 4:
+                return LevelOfKnowledge.Native;
+            default:
+                // Handle the case where the selected position is out of range
+                // You can throw an exception, return a default value, or handle it based on your requirements
+                // For example, return Amateur as a default value
+                return LevelOfKnowledge.Amateur;
+        }
     }
 }
