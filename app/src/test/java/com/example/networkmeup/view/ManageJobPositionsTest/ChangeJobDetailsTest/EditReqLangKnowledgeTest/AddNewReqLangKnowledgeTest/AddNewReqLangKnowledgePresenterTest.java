5b209dd20d1b8c5b385389201ecbee57544f23fa
apokamp <apo.kampouris.2003@gmail.com>
2024-01-07 20:34:38 +0200

    AddNewReqLangKnowledge fully covered presenter test (100%)

--- app/src/test/java/com/example/networkmeup/view/ManageJobPositionsTest/ChangeJobDetailsTest/EditReqLangKnowledgeTest/AddNewReqLangKnowledgeTest/AddNewReqLangKnowledgePresenterTest.java
+++ app/src/test/java/com/example/networkmeup/view/ManageJobPositionsTest/ChangeJobDetailsTest/EditReqLangKnowledgeTest/AddNewReqLangKnowledgeTest/AddNewReqLangKnowledgePresenterTest.java
@@ -1,4 +1,56 @@
 package com.example.networkmeup.view.ManageJobPositionsTest.ChangeJobDetailsTest.EditReqLangKnowledgeTest.AddNewReqLangKnowledgeTest;
 
+import com.example.networkmeup.dao.Initializer;
+import com.example.networkmeup.daoMemory.EmployerDAOMemory;
+import com.example.networkmeup.daoMemory.MemoryInitializer;
+import com.example.networkmeup.domain.Email;
+import com.example.networkmeup.domain.Employer;
+import com.example.networkmeup.domain.LevelOfKnowledge;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.AddNewReqLangKnowledge.AddNewReqLangKnowledgePresenter;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqWorkExperience.AddNewReqWorkExperience.AddNewReqWorkExperiencePresenter;
+import com.example.networkmeup.view.ManageJobPositionsTest.ChangeJobDetailsTest.EditReqWorkExperienceTest.AddNewReqWorkExperienceTest.AddNewReqWorkExpViewStub;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class AddNewReqLangKnowledgePresenterTest {
+    private AddNewReqLangKnowledgePresenter presenter;
+    private AddNewReqLangKnowledgeViewStub stub;
+    private Initializer initializer;
+    @Before
+    public void setup(){
+        initializer = new MemoryInitializer();
+        initializer.prepareData();
+        stub = new AddNewReqLangKnowledgeViewStub();
+        presenter = new AddNewReqLangKnowledgePresenter(stub, "b.be@northfreedom.com", new EmployerDAOMemory().getByEmail(new Email("b.be@northfreedom.com")).getJobs().get(0));
+    }
+
+    @Test
+    public void checkAddLanguageKnowledge(){
+        //attributes set to simulated activity widgets
+        stub.setDescription("Description Test");//Description text
+        stub.setLanguage(0); //English
+        stub.setLevelOfKnowledge(LevelOfKnowledge.Amateur); //LevelOfKnowledge
+
+        presenter.onAdd(); //new Language Knowledge to be added (second for this job)
+
+        Employer currEmployer = new EmployerDAOMemory().getByEmail(new Email("b.be@northfreedom.com"));
+
+        //testing view side data
+        Assert.assertEquals("Required Work Experience was successfully created!", stub.getLastDialogMessage());
+        Assert.assertEquals("b.be@northfreedom.com", stub.getLastTokenPassed());
+        Assert.assertEquals(true, stub.getLastPassedJob().equals(currEmployer.getJobs().get(0)));
+
+        //testing database side data
+        //check if req Language Knowledge was actually added
+        Assert.assertEquals(2, currEmployer.getJobs().get(0).getReqLanguageKnowledge().size());
+
+        //check if req Language Knowledge has the right attributes
+
+        Assert.assertEquals("Description Test", currEmployer.getJobs().get(0).getReqLanguageKnowledge().get(1).getDescription());
+        Assert.assertEquals("English", currEmployer.getJobs().get(0).getReqLanguageKnowledge().get(1).getLanguage().getLanguage());
+        Assert.assertEquals(LevelOfKnowledge.Amateur, currEmployer.getJobs().get(0).getReqLanguageKnowledge().get(1).getLvlOfKnowledge());
+
+    }
 }
--- app/src/test/java/com/example/networkmeup/view/ManageJobPositionsTest/ChangeJobDetailsTest/EditReqLangKnowledgeTest/AddNewReqLangKnowledgeTest/AddNewReqLangKnowledgeViewStub.java
+++ app/src/test/java/com/example/networkmeup/view/ManageJobPositionsTest/ChangeJobDetailsTest/EditReqLangKnowledgeTest/AddNewReqLangKnowledgeTest/AddNewReqLangKnowledgeViewStub.java
@@ -1,4 +1,59 @@
 package com.example.networkmeup.view.ManageJobPositionsTest.ChangeJobDetailsTest.EditReqLangKnowledgeTest.AddNewReqLangKnowledgeTest;
 
-public class AddNewReqLangKnowledgeViewStub {
+import com.example.networkmeup.domain.Job;
+import com.example.networkmeup.domain.LevelOfKnowledge;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.AddNewReqLangKnowledge.AddNewReqLangKnowledgeView;
+
+public class AddNewReqLangKnowledgeViewStub implements AddNewReqLangKnowledgeView {
+    private String description;
+    private LevelOfKnowledge levelOfKnowledge;
+    private int language;
+    private String lastTokenPassed;
+    private String lastDialogMessage;
+    private Job lastPassedJob;
+    @Override
+    public void successfulAdd(String message, String userToken, Job job) {
+        lastDialogMessage = message;
+        lastTokenPassed = userToken;
+        lastPassedJob = job;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public int getLanguage() {
+        return language;
+    }
+
+    @Override
+    public LevelOfKnowledge getLevelOfKnowledge() {
+        return levelOfKnowledge;
+    }
+
+    public String getLastTokenPassed() {
+        return lastTokenPassed;
+    }
+
+    public String getLastDialogMessage() {
+        return lastDialogMessage;
+    }
+
+    public Job getLastPassedJob() {
+        return lastPassedJob;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLevelOfKnowledge(LevelOfKnowledge LevelOfKnowledge) {
+        this.levelOfKnowledge = LevelOfKnowledge;
+    }
+
+    public void setLanguage(int Language) {
+        this.language = Language;
+    }
 }
