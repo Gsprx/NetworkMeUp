950cfeae0370e186d46a62ea531378ada674106c
apokamp <apo.kampouris.2003@gmail.com>
2024-01-07 11:05:01 +0200

    ChangeNewLangKnowledgeDetailsPresenter

--- app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/ChangeReqLangKnowledgeDetails/ChangeReqLangKnowledgeDetailsPresenter.java
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/ChangeReqLangKnowledgeDetails/ChangeReqLangKnowledgeDetailsPresenter.java
@@ -1,4 +1,38 @@
 package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.ChangeReqLangKnowledgeDetails;
 
+import com.example.networkmeup.daoMemory.ExpertiseAreaDAOMemory;
+import com.example.networkmeup.daoMemory.LanguageDAOMemory;
+import com.example.networkmeup.domain.Education;
+import com.example.networkmeup.domain.Job;
+import com.example.networkmeup.domain.LanguageKnowledge;
+import com.example.networkmeup.domain.LevelOfKnowledge;
+import com.example.networkmeup.domain.LevelOfStudies;
+import com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqEducation.ChangeReqEducationDetails.ChangeReqEducationDetailsView;
+
 public class ChangeReqLangKnowledgeDetailsPresenter {
+    private ChangeReqLangKnowledgeDetailsView view;
+    private String userToken;
+    private Job job;
+
+    public ChangeReqLangKnowledgeDetailsPresenter(ChangeReqLangKnowledgeDetailsView view, String userToken, Job job) {
+        this.view = view;
+        this.userToken = userToken;
+        this.job = job;
+    }
+
+    public void onSave(int position){
+        LanguageKnowledge updatedLanguageKnowledge = job.getReqLanguageKnowledge().get(position);
+
+        updatedLanguageKnowledge.setLvlOfKnowledge(LevelOfKnowledge.values()[view.getLevelOfKnowledge().ordinal()]);
+        updatedLanguageKnowledge.setDescription(view.getDescription());
+        updatedLanguageKnowledge.setLanguage(new LanguageDAOMemory().getAll().get(view.getLanguage()));
+
+        view.successfulSave("Language Knowledge was successfully updated!", userToken, job);
+    }
+
+    public void onDelete(int position){
+        job.getReqLanguageKnowledge().remove(position);
+
+        view.successfulDelete("Language Knowledge was successfully deleted!", userToken, job);
+    }
 }
