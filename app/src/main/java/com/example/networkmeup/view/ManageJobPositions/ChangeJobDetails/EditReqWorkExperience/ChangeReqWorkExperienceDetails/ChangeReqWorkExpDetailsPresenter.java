768637cdea8eef7ed26077c8aad9934f81214f1f
George <kroustalgeorgio@gmail.com>
2024-01-07 13:23:24 +0200

    EditReqworkExp4

---
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqWorkExperience/ChangeReqWorkExperienceDetails/ChangeReqWorkExpDetailsPresenter.java
@@ -0,0 +1,34 @@
+package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqWorkExperience.ChangeReqWorkExperienceDetails;
+
+import com.example.networkmeup.daoMemory.ExpertiseAreaDAOMemory;
+
+import com.example.networkmeup.domain.Job;
+import com.example.networkmeup.domain.WorkExperience;
+
+public class ChangeReqWorkExpDetailsPresenter {
+    private ChangeReqWorkExperienceDetailsView view;
+    private String userToken;
+    private Job job;
+
+    public ChangeReqWorkExpDetailsPresenter(ChangeReqWorkExperienceDetailsView view, String userToken, Job job) {
+        this.view = view;
+        this.userToken = userToken;
+        this.job = job;
+    }
+
+    public void onSave(int position){
+        WorkExperience updatedWorkExp = job.getReqWorkExperience().get(position);
+
+        updatedWorkExp.setYears(view.getYears());
+        updatedWorkExp.setDescription(view.getDescription());
+        updatedWorkExp.setExpArea(new ExpertiseAreaDAOMemory().getAll().get(view.getExpertiseArea()));
+
+        view.successfulSave("Work Experience was successfully updated!", userToken, job);
+    }
+
+    public void onDelete(int position){
+        job.getReqWorkExperience().remove(position);
+
+        view.successfulDelete("Work Experience was successfully deleted!", userToken, job);
+    }
+}
