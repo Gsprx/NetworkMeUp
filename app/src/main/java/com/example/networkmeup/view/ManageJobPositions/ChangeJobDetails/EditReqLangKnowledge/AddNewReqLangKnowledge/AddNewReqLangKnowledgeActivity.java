93bc6534433418ead42e2993b4f0483890ccddbd
apokamp <apo.kampouris.2003@gmail.com>
2024-01-07 09:51:22 +0200

    Basic file creation of the AddNewReqLangKnowlgedge (Activity, Presenter, View, XML)

---
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/AddNewReqLangKnowledge/AddNewReqLangKnowledgeActivity.java
@@ -0,0 +1,16 @@
+package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.AddNewReqLangKnowledge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.networkmeup.R;
+
+public class AddNewReqLangKnowledgeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_req_lang_knowledge);
+    }
+}
