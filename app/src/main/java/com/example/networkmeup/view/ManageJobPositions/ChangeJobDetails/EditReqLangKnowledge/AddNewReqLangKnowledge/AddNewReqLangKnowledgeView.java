e22fa55f2c0712e0b05a851319df4f5ab00ec63e
apokamp <apo.kampouris.2003@gmail.com>
2024-01-07 10:16:47 +0200

    AddNewReqLangKnowledgeView

--- app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/AddNewReqLangKnowledge/AddNewReqLangKnowledgeView.java
+++ app/src/main/java/com/example/networkmeup/view/ManageJobPositions/ChangeJobDetails/EditReqLangKnowledge/AddNewReqLangKnowledge/AddNewReqLangKnowledgeView.java
@@ -1,4 +1,11 @@
 package com.example.networkmeup.view.ManageJobPositions.ChangeJobDetails.EditReqLangKnowledge.AddNewReqLangKnowledge;
 
+import com.example.networkmeup.domain.LevelOfKnowledge;
+
 public interface AddNewReqLangKnowledgeView {
+        void successfulAdd(String message, String userToken);
+        String getDescription();
+        public int getLanguage();
+        public LevelOfKnowledge getLevelOfKnowledge();
+
 }
--- app/src/main/java/com/example/networkmeup/view/ModifyCV/ModifyCVEditLanguageKnowledge/AddNewLanguageKnowledge/AddNewLanguageKnowledgeView.java
+++ app/src/main/java/com/example/networkmeup/view/ModifyCV/ModifyCVEditLanguageKnowledge/AddNewLanguageKnowledge/AddNewLanguageKnowledgeView.java
@@ -6,10 +6,6 @@ import com.example.networkmeup.domain.LevelOfKnowledge;
 public interface AddNewLanguageKnowledgeView {
     public void successfulAdd(String message, String userToken);
     public String getDescription();
-
-    /*
-     * Integers for the positions in the list/enum
-     */
     public int getLanguage();
     public LevelOfKnowledge getLevelOfKnowledge();
 }
