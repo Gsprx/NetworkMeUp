e17c9417944f45f6ad73c72523d59db49b8b8fb8
apokamp <apo.kampouris.2003@gmail.com>
2024-01-08 13:19:33 +0200

    EditAccountEmplyerView changes

--- app/src/main/java/com/example/networkmeup/view/EditAccountEmployer/EditAccountEmployerView.java
+++ app/src/main/java/com/example/networkmeup/view/EditAccountEmployer/EditAccountEmployerView.java
@@ -1,13 +1,14 @@
 package com.example.networkmeup.view.EditAccountEmployer;
 
 import com.example.networkmeup.domain.Email;
-import com.example.networkmeup.domain.Employee;
 import com.example.networkmeup.domain.Employer;
 import com.example.networkmeup.domain.Password;
 import com.example.networkmeup.domain.Phone;
 import com.example.networkmeup.domain.TIN;
 
 public interface EditAccountEmployerView {
+    public String getCompanyName() throws RuntimeException;
+    public String getSector() throws RuntimeException;
     public Email getEmail() throws RuntimeException;
 
     public Password getPassword() throws RuntimeException;
