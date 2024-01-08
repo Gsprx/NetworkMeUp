c9139dcc4410136b92a85e5bdab989ea93745b58
George <kroustalgeorgio@gmail.com>
2024-01-08 18:01:20 +0200

    TestsEditAccountEmployee

--- app/src/main/java/com/example/networkmeup/view/EditAccountEmployee/ShowApplicationsEmployee/ShowApplicationsEmployeeActivity.java
+++ app/src/main/java/com/example/networkmeup/view/EditAccountEmployee/ShowApplicationsEmployee/ShowApplicationsEmployeeActivity.java
@@ -7,16 +7,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.networkmeup.R;
-import com.example.networkmeup.dao.EmployeeDAO;
-import com.example.networkmeup.daoMemory.EmployeeDAOMemory;
 import com.example.networkmeup.domain.Application;
-import com.example.networkmeup.domain.Email;
 import com.example.networkmeup.domain.Employee;
 import com.example.networkmeup.utils.RecyclerViewAdapters.EmployeeApplicationRecyclerViewAdapter;
 import com.example.networkmeup.view.EditAccountEmployee.EditAccountEmployeeActivity;
-import com.example.networkmeup.view.HomeEmployee.HomeEmployeeActivity;
 
 import java.util.ArrayList;
 
@@ -37,12 +35,14 @@ public class ShowApplicationsEmployeeActivity extends AppCompatActivity implemen
             userEmail = null;
         }
 
-        final ShowApplicationPresenter presenter = new ShowApplicationPresenter(this,userEmail);
+        final ShowApplicationsPresenter presenter = new ShowApplicationsPresenter(this,userEmail);
         //get employee data to pass to recycler view
         ArrayList<Application> ApplicationList = presenter.getApplications();
 
         //get recycler view reference
         RecyclerView recyclerView = findViewById(R.id.recyclerViewEditAccountEmployee);
+
+
         //create Adapter
         EmployeeApplicationRecyclerViewAdapter applicationRecycler = new EmployeeApplicationRecyclerViewAdapter(this,ApplicationList);
         recyclerView.setAdapter(applicationRecycler);
